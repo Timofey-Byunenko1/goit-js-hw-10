0f06576dc7928288589df0cd25e37b1b0cc5722e
Timofey-Byunenko1 <byunenkotimofey@gmail.com>
2024-11-25 13:38:15 +0200

    add index file

---
+++ src/js/2-snkalbar.js
@@ -0,0 +1,44 @@
+// Імпорт бібліотеки iziToast через CDN уже включений в HTML.
+
+document.querySelector('.form').addEventListener('submit', (event) => {
+    event.preventDefault(); // Зупиняємо стандартну поведінку форми
+  
+
+    const delayInput = event.target.elements.delay.value;
+    const delay = parseInt(delayInput, 10);
+  
+
+    const state = event.target.elements.state.value;
+  
+    // Створюємо проміс
+    createPromise(delay, state)
+      .then((delay) => {
+        // Виведення успішного повідомлення
+        iziToast.success({
+          title: '✅ Success',
+          message: `Fulfilled promise in ${delay}ms`,
+          position: 'topRight',
+        });
+      })
+      .catch((delay) => {
+    
+        iziToast.error({
+          title: '❌ Error',
+          message: `Rejected promise in ${delay}ms`,
+          position: 'topRight',
+        });
+      });
+  });
+  
+  // Функція для створення промісу
+  function createPromise(delay, state) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (state === 'fulfilled') {
+          resolve(delay); // Успіх
+        } else {
+          reject(delay); // Помилка
+        }
+      }, delay);
+    });
+  }
